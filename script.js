9b2cd281c506deceb82be488e218c3838a3fbd2c
Hriddhima Chakraborty <124462729+Hriddhima-25@users.noreply.github.com>
2024-02-27 17:06:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.onscroll = () => {
         let height = sec.offsetHeight
         let id = sec.getAttribute('id')
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active')
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
@@ -42,10 +42,10 @@ ScrollReveal({
     delay: 200
 });
 
-ScrollReveal().reveal('.left', {origin: 'left'})
-ScrollReveal().reveal('.right', {origin: 'right'})
-ScrollReveal().reveal('.top', {origin: 'top'})
-ScrollReveal().reveal('.bottom', {origin: 'bottom'})
+ScrollReveal().reveal('.left', { origin: 'left' })
+ScrollReveal().reveal('.right', { origin: 'right' })
+ScrollReveal().reveal('.top', { origin: 'top' })
+ScrollReveal().reveal('.bottom', { origin: 'bottom' })
 
 
 const typed = new Typed('.multiple-text', {
@@ -55,3 +55,102 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 })
+
+// ================receive data via email==================
+const form = document.querySelector("form");
+const fullname = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("emailsub");
+const message = document.getElementById("message");
+
+function sendEmail() {
+
+    const bodymessage = `Full Name: ${fullname.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${message.value}`;
+
+    Email.send({
+        SecureToken :"5aa491dd-757d-4a03-9083-4d25bbdb8677 ",
+        To: 'hriddhimachakraborty@gmail.com',
+        From: "hriddhimachakraborty@gmail.com",
+        Subject: subject.value,
+        Body: bodymessage
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Message sent sucessfully!!",
+                    icon: "success"
+                });
+            }
+        }
+    );
+}
+
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        })
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+
+        });
+    }
+}
+
+
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorTxtEmail.innerText = "Enter valid email address";
+        }
+        else {
+            errorTxtEmail.innerText = "Email address can't be blank";
+        }
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if(!fullname.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("errors") && !subject.classList.contains("error") && !message.classList.contains("error")) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
+});
